1fd1e7cadaedd882b3d60b1920b4b9569ae9058d
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-01-06 15:24:17 -1000

    all tests pass

--- vault.js
+++ vault.js
@@ -1,4 +1,25 @@
 'use strict';
 module.exports = function() {
+    let _vault = {};
+    
+    function setValue(key, value) {
+            _vault[key] = value;
+  
+    }
+
+    function getValue(key) {
+        if (!key){
+            return null;
+        }else if (!_vault.hasOwnProperty(key)){
+            return null;
+        }else{
+            return _vault[key];
+        }
+    }
+
+    return {
+        setValue: setValue,
+        getValue: getValue
+    }
   
 };
